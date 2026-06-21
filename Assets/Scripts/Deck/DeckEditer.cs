ac5ec750f344bcba1ee2fc63427f2e2ab02b667d
sevan94 <evanseo888@gmail.com>
2026-06-21 19:50:26 +0900

    전체카드와 보유카드 추가

--- Assets/Scripts/Deck/DeckEditer.cs
+++ Assets/Scripts/Deck/DeckEditer.cs
@@ -6,9 +6,8 @@ public class DeckEditer : MonoBehaviour
 {
     [Header("Deck")]
     [SerializeField] private DeckData deckData; //현재 덱
-    //전체 카드(없어도 됨)
-    //보유한 카드
-    
+    [SerializeField] private List<CardData> wholeCard; //전체 카드(없어도 됨)
+    [SerializeField] private List<CardData> ownedCard; //보유한 카드
 
     // Start is called before the first frame update
     void Start()
